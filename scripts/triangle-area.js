f651938951a0f54e939f91ce5d1ad5154fa8164e
hamed18 <hamedcnec@gmail.com>
2024-04-20 00:11:17 +0600

    dom and event handler for triangle area

--- scripts/triangle-area.js
+++ scripts/triangle-area.js
@@ -1,3 +1,10 @@
 function CalculateTriangleArea(){
-	console.log("inside function");
+// Create a DOM that access the input field in UI to strore the input data
+	const TriangleBase = document.getElementById('triangle-base');
+	const TriangleHeight = document.getElementById('triangle-height');
+	const Area = 0.5*parseFloat(TriangleBase.value)*parseFloat(TriangleHeight.value); // .value bcz default dom takes html layout and parseFloat bcz in default input is string
+
+// now show the calculated area in UI
+const AreaValue = document.getElementById('triangle-area');
+AreaValue.innerText = Area;  // memorize
 }
